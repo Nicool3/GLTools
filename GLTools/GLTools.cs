5f14bde7b71549fbbe7626001686bdc191bf9356
sunkaiqi <sunkaiqi@SUNKAIQI-PC>
2021-05-18 20:48:45 +0800

    添加项目文件。

---
+++ GLTools/GLTools.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+
+
+namespace GLTools
+{
+    public static class GLTools
+    {
+        /// <summary>
+        /// 绘制钢筋
+        /// </summary>
+        [CommandMethod("DrawGJ")]
+        public static void DrawGJ()
+        {
+            // 获取当前文档和数据库
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            Database db = doc.Database;
+
+            Point3d P0 = new Point3d();
+            P0 = ed.GetPointOnScreen("请指定圆心: ");
+
+            db.AddCircleModeSpace(P0, 100);
+            ed.WriteMessage("绘制完成");
+
+        }
+    }
+}
---
+++ GLTools/GetObjectTools.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.AutoCAD.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace GLTools
+{
+    public static partial class GetObjectTools
+    {
+        /// <summary>
+        /// 屏幕取点
+        /// </summary>
+        public static Point3d GetPointOnScreen(this Editor ed, string opt)
+        {
+            PromptPointResult PPtRes;
+            PromptPointOptions PPtOpts = new PromptPointOptions("");
+            PPtOpts.Message = opt;
+            PPtRes = ed.GetPoint(PPtOpts);
+            return PPtRes.Value;
+        }
+
+    }
+}
